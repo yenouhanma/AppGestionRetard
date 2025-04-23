01679a74b31fd057ca2e26615b9d75f89912ca21
yenouhanma <yenouhanmamano@gmail.com>
2025-04-23 19:45:09 +0000

    Authentification connexion

---
+++ src/screens/Home/DashboardScreen.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function DashboardScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Dashboard Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+  },
+  text: {
+    fontSize: 24,
+    color: '#333',
+  },
+});
